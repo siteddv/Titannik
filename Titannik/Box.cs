791cfd5acca107d0ca13348e9f912edcd6cff41f
Dimitrii Vinogradov <v-dimitriiv@microsoft.com>
2023-11-10 20:54:24 +0600

    started OOP

---
+++ Titannik/Box.cs
@@ -0,0 +1,23 @@
+﻿namespace Titannik
+{
+    internal class Box
+    {
+        public double Width { get; private set; }
+        double height;
+        double length;
+
+        public void SetDefaultValues()
+        {
+            width = 100;
+            height = 110;
+            length = 130;
+        }
+
+        public void PrintBoxSizes()
+        {
+            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Height: " + height);
+            Console.WriteLine("Length: " + length);
+        }
+    }
+}
