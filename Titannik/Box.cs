c5c278ea1e4dcf83ffb00e40608c4960c296752c
Dimitrii Vinogradov <v-dimitriiv@microsoft.com>
2023-11-13 20:57:52 +0600

    constructors

--- Titannik/Box.cs
+++ Titannik/Box.cs
@@ -4,64 +4,33 @@
     {
         public Box()
         {
-            Console.WriteLine("ya rodivsya");
-            Console.WriteLine("ya rodivsya");
-            Console.WriteLine("ya rodivsya");
-            Console.WriteLine("ya rodivsya");
-            Console.WriteLine("ya rodivsya");
+
         }
 
-        public double Width
+        public Box(double width, double height, double length)
         {
-            get
-            {
-                Console.WriteLine("Width is got");
-                Console.WriteLine("Fuck you Tony");
-                return _width;
-            }
-            set
-            {
-                if(value < 0)
-                {
-                    _width = 0;
-                    Console.WriteLine("you entered incorrect value");
-                    return;
-                }
-
-                _width = value;
-            }
+            Width = width;
+            Height = height;
+            Length = length;
         }
-        private double _width;
 
-        double height;
-        double length;
-
-        public int Age { get; set; }
-
-        public int Age1 { 
-            get 
-            {
-                return _age1;
-            }
-            set
-            {
-                _age1 = value;
-            } 
-        }
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Length { get; set; }
 
         private int _age1;
         public void SetDefaultValues()
         {
             Width = 100;
-            height = 110;
-            length = 130;
+            Height = 110;
+            Length = 130;
         }
 
         public void PrintBoxSizes()
         {
             Console.WriteLine("Width: " + Width);
-            Console.WriteLine("Height: " + height);
-            Console.WriteLine("Length: " + length);
+            Console.WriteLine("Height: " + Height);
+            Console.WriteLine("Length: " + Length);
         }
     }
 }
--- Titannik/Program.cs
+++ Titannik/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Titannik;
 
 Box box = new Box();
-box.SetDefaultValues();
-box.PrintBoxSizes();
+box.Width = 50;
+box.Height = 60;
+box.Length = 70;
+
+Box box123 = new Box(50, 60, 70);
