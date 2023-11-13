ba81ef6db1ba611103c7db472b9d28cf949236d2
Dimitrii Vinogradov <v-dimitriiv@microsoft.com>
2023-11-13 20:48:15 +0600

    add examples for properties and ctrs

--- Titannik/Box.cs
+++ Titannik/Box.cs
@@ -2,20 +2,64 @@
 {
     internal class Box
     {
-        public double Width { get; private set; }
+        public Box()
+        {
+            Console.WriteLine("ya rodivsya");
+            Console.WriteLine("ya rodivsya");
+            Console.WriteLine("ya rodivsya");
+            Console.WriteLine("ya rodivsya");
+            Console.WriteLine("ya rodivsya");
+        }
+
+        public double Width
+        {
+            get
+            {
+                Console.WriteLine("Width is got");
+                Console.WriteLine("Fuck you Tony");
+                return _width;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    _width = 0;
+                    Console.WriteLine("you entered incorrect value");
+                    return;
+                }
+
+                _width = value;
+            }
+        }
+        private double _width;
+
         double height;
         double length;
 
+        public int Age { get; set; }
+
+        public int Age1 { 
+            get 
+            {
+                return _age1;
+            }
+            set
+            {
+                _age1 = value;
+            } 
+        }
+
+        private int _age1;
         public void SetDefaultValues()
         {
-            width = 100;
+            Width = 100;
             height = 110;
             length = 130;
         }
 
         public void PrintBoxSizes()
         {
-            Console.WriteLine("Width: " + width);
+            Console.WriteLine("Width: " + Width);
             Console.WriteLine("Height: " + height);
             Console.WriteLine("Length: " + length);
         }
--- Titannik/Program.cs
+++ Titannik/Program.cs
@@ -2,6 +2,4 @@
 
 Box box = new Box();
 box.SetDefaultValues();
-Console.WriteLine(box.GetWidth());
-//Console.WriteLine(box.width); // ok
 box.PrintBoxSizes();
